0c49a7894400fbf263b7862a18da9fd6d8b2c158
Sasha Murzin <murzin@gmail.com>
2026-07-26 12:23:58 +0400

    som changes in DDl, just for case

--- DDL/ddl.sql
+++ DDL/ddl.sql
@@ -19,15 +19,20 @@ CREATE TABLE IF NOT EXISTS events (
     evt_to   TEXT NOT NULL,
     etp_id INTEGER NOT NULL,
     epl_id INTEGER NOT NULL,
-    FOREIGN KEY (etp_id) REFERENCES event_types(etp_id) ON DELETE CASCADE,
-    FOREIGN KEY (epl_id) REFERENCES event_places(epl_id) ON DELETE CASCADE
+    FOREIGN KEY (etp_id) REFERENCES event_types(etp_id),
+    FOREIGN KEY (epl_id) REFERENCES event_places(epl_id),
+    FOREIGN KEY (eer_id) REFERENCES event_era(eel_id)
 );
 
 CREATE TABLE IF NOT EXISTS event_links (
     evt_id_1 INTEGER NOT NULL,
     evt_id_2 INTEGER NOT NULL,
     PRIMARY KEY (evt_id_1, evt_id_2),
-    FOREIGN KEY (evt_id_1) REFERENCES events(evt_id) ON DELETE CASCADE,
-    FOREIGN KEY (evt_id_2) REFERENCES events(evt_id) ON DELETE CASCADE
+    FOREIGN KEY (evt_id_1) REFERENCES events(evt_id),
+    FOREIGN KEY (evt_id_2) REFERENCES events(evt_id)
 );
 
+CREATE TABLE IF NOT EXISTS event_era (
+    eer_id INTEGER PRIMARY KEY,
+    eer_name TEXT NOT NULL
+);
